007e834fbbba35ce8386551a4076b2018c73faad
foreverzzzzzzz <15834063946@163.com>
2026-06-03 17:23:30 +0800

    固定不动的敌人

--- Assets/Scripts/实践小项目/Game/GameScene/Weapon/BulletObj.cs
+++ Assets/Scripts/实践小项目/Game/GameScene/Weapon/BulletObj.cs
@@ -26,9 +26,20 @@ public class BulletObj : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Cube"))
+        //子弹 设计到立方体 会爆炸 
+        //同样 子弹设计到 不同阵营的对象也应该爆炸
+        if( other.CompareTag("Cube") ||
+            other.CompareTag("Player") && fatherObj.CompareTag("Monster") ||
+            other.CompareTag("Monster") && fatherObj.CompareTag("Player"))
         {
-            
+            //判断是否受伤
+            //得到碰撞到的对象身上 是否有坦克相关脚本 我们用里氏替换原则
+            //通过父类去获取
+            TankBaseObj obj = other.GetComponent<TankBaseObj>();
+            //判断 对象上 是否挂载了 坦克相关脚本 
+            if (obj != null)
+                obj.Wound(fatherObj);
+
             //当子弹销毁时 可以创建一个 爆炸特效
             if(effObj != null)
             {
@@ -42,10 +53,10 @@ public class BulletObj : MonoBehaviour
                 audioS.mute = !GameDataMgr.Instance.musicData.isOpenSound;
             }
 
-            
             Destroy(this.gameObject);
         }
     }
+
     
     //设置拥有者
     public void SetFather(TankBaseObj obj)
